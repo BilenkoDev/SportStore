9991a36feeb56dbde23aba0b0b87944ba3aee296
Oleg Bilenko <obilenko@hotmail.ca>
2024-09-04 00:24:20 -0400

    Section 2, Solution, API, Database created

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}", Name = "GetProduct")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null) return NotFound();
+
+            return product;
+        }
+    }
+}
---
+++ API/Data/StoreContext.cs
@@ -0,0 +1,16 @@
+using API.Entities;
+//using API.Entities.OrderAggregate;
+using Microsoft.AspNetCore.Identity;
+//using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class StoreContext : DbContext
+{
+    public StoreContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+}
